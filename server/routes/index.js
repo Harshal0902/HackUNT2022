c64c28d0d94c940f61b24cc42e706ee32fbdbc36
Harshal0902 <harshalraikwar07@gmail.com>
2022-04-24 01:47:33 +0530

    ✨ Add meeting rooms

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-const authRoutes = require("./auth");
-const userRoutes = require("./users");
-const meetingRoutes = require("./meetings");
-
-module.exports = function ({ app, dbConn }) {
-  authRoutes({ app, dbConn });
-  userRoutes({ app, dbConn });
-  meetingRoutes({ app, dbConn });
-};
