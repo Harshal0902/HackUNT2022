73f02d651c9580e1dd6597f109fa1881876f2f00
Harshal0902 <harshalraikwar07@gmail.com>
2022-04-24 13:42:31 +0530

    💄 Add modal and update UI

--- client/src/components/VideoPlayer.js
+++ client/src/components/VideoPlayer.js
@@ -1,15 +1,15 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Grid, Typography, Paper, makeStyles } from '@material-ui/core';
 import { SocketContext } from '../context/Context';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 import fetch from "node-fetch";
-import { useState } from 'react';
+import { HiX } from "react-icons/hi";
 
 const useStyles = makeStyles((theme) => ({
     video: {
-        width: '550px',
+        width: '400px',
         [theme.breakpoints.down('xs')]: {
-            width: '300px',
+            width: '250px',
         },
     },
     gridContainer: {
@@ -27,7 +27,8 @@ const useStyles = makeStyles((theme) => ({
 
 const VideoPlayer = () => {
 
-    
+    const [showModal, setShowModal] = useState(false);
+
     const { name, callAccepted, myVideo, userVideo, callEnded, stream, call } = useContext(SocketContext);
     const classes = useStyles();
 
@@ -40,30 +41,29 @@ const VideoPlayer = () => {
 
     const [ex, setEx] = useState(0);
     const [ais, setAis] = useState([])
-    const [question, setQuestion] = useState("")
 
     if (!browserSupportsSpeechRecognition) {
         return <span>Browser doesn't support speech recognition.</span>;
     }
 
-    function actionItem() {
+    function actionItem(arg) {
         console.log("AI")
-        setQuestion("")
-        // if (arg === 1) {
-            // }
-            // if (arg === 3) {
-                // inArg = "I think you should try to worry less about other people's opinions of you and I would encourage you to remind yourself of how you're a great asset to the team just think about how you got hired here over many other candidates for a reason."
-                // inArg = "I think you should really start taking breaks for example I suggest that every hour go take a walk around the office also make sure to communicate with the team when you're feeling overloaded so we can share the work."
-        // }
+        let inArg
+        if (arg === 1) {
+            inArg = "I think you should really start taking breaks for example I suggest that every hour go take a walk around the office also make sure to communicate with the team when you're feeling overloaded so we can share the work."
+        }
+        if (arg === 3) {
+            inArg = "I think you should try to worry less about other people's opinions of you and I would encourage you to remind yourself of how you're a great asset to the team just think about how you got hired here over many other candidates for a reason."
+        }
 
         // const x = "adfds+fsdf-sdf";
         const separators = ["I think you should really", "for example I suggest that", "also make sure to", "I think you should try to", "and I would encourage you to", "just"];
-        const tokens = transcript.split(new RegExp(separators.join('|'), 'g'));
+        const tokens = inArg.split(new RegExp(separators.join('|'), 'g'));
         tokens.forEach(token => {
             if (token.length > 1) {
                 setAis(curr => [...curr, token])
             }
-        })        
+        })
     }
     async function submitTranscript() {
         setAis([])
@@ -74,20 +74,20 @@ const VideoPlayer = () => {
         // A: I feel like my work life balance has really taken a toll since starting this job Q: how do you feel about your WLB
         let modTranscript
         if (ex === 0) {
-            modTranscript = "<answer> " + "<context>" + transcript 
+            modTranscript = "<answer> " + "<context>" + transcript
             setEx(1)
         }
         else if (ex === 1) {
-            actionItem()
+            actionItem(1)
             setEx(2)
             return
         }
         else if (ex === 2) {
-            modTranscript = "<answer> I feel like my work life balance has really taken a toll since starting this job " + "<context>" + transcript 
+            modTranscript = "<answer> I feel like my work life balance has really taken a toll since starting this job " + "<context>" + transcript
             setEx(3)
         }
         else if (ex === 3) {
-            actionItem()
+            actionItem(3)
             return
         }
         console.log(modTranscript)
@@ -108,54 +108,85 @@ const VideoPlayer = () => {
         console.log(body)
         const result = await response.json();
         console.log(result)
-        let answer = result[0].generated_text;
-        answer = answer.replace(/[?=]/g, '');
-        answer = answer + "?"
-        answer = answer.replace("you", "they")
-        answer = answer.replace("your", "their")
-        answer = answer.replace("I", "they")
-        answer = answer.replace("my", "their")
+        const answer = result[0].generated_text;
         console.log(answer);
-        setQuestion(answer);
-        resetTranscript()
         return answer;
     }
 
     return (
-        <div>
-            <Grid container className={classes.gridContainer}>
+
+        <div className='bg-gray-400 rounded-md p-4'>
+            <div className='flex flex-row' >
                 {stream && (
                     <Paper className={classes.paper}>
-                        <Grid item xs={12} md={6}>
+                        <div className="grid place-items-center" >
                             <Typography variant="h5" gutterBottom>{name || 'Name'}</Typography>
                             <video playsInline muted ref={myVideo} autoPlay className={classes.video} />
-                        </Grid>
+                        </div>
                     </Paper>
                 )}
                 {callAccepted && !callEnded && (
                     <Paper className={classes.paper}>
-                        <Grid item xs={12} md={6}>
+                        <div className="grid place-items-center" >
                             <Typography variant="h5" gutterBottom>{call.name || 'Name'}</Typography>
                             <video playsInline ref={userVideo} autoPlay className={classes.video} />
-                        </Grid>
+                        </div>
                     </Paper>
                 )}
 
-            </Grid>
+            </div>
 
-            <div className='flex justify-center items-center flex-col text-white'>
+            <div className='flex justify-center items-center flex-col'>
                 <div className='text-2xl'>
                     <p>Microphone: {listening ? 'on' : 'off'}</p>
                 </div>
-                <div className="flex flex-row space-x-3 my-2">
+                <div className="flex flex-row space-x-3 my-2 text-white">
                     <button className='bg-secondary px-3 py-2 rounded-md' onClick={SpeechRecognition.startListening}>Start</button>
                     <button className='bg-secondary px-3 py-2 rounded-md' onClick={SpeechRecognition.stopListening}>Stop</button>
                     <button className='bg-secondary px-3 py-2 rounded-md' onClick={resetTranscript}>Reset</button>
                 </div>
                 <p className='text-xl'>{transcript}</p>
-                <p>{question} </p>
-                <button onClick={submitTranscript}> Submit</button>
-                {ais.map(ai => {return <div>-{ai}</div>})}
+
+                <div>
+                    <button className='my-2 px-4 py-2 bg-secondary text-white text-2xl rounded-2xl' onClick={() => setShowModal(true)}> Submit</button>
+
+                    {/* <button className='my-2 px-4 py-2 bg-secondary text-white text-2xl rounded-2xl'
+                        onClick={submitTranscript}
+                    > Submit</button> */}
+                </div>
+
+                {showModal ? (
+                    <div>
+                        <div className="flex overflow-x-hidden mr-20 w-screen items-center justify-center h-screen overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                        >
+                            <div className="relative my-6 mx-auto w-96">
+                                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-secondary outline-none focus:outline-none">
+                                    <div className="flex items-start justify-between p-5 border-solid rounded-t text-white">
+                                        <div className="text-2xl font-base tracking-wide">
+                                            Answer Quetions
+                                        </div>
+
+                                        <button className="absolute right-6" onClick={() => setShowModal(false)} aria-hidden="false" aria-label="button">
+                                            <HiX className="h-7 w-7" aria-hidden="false" />
+                                        </button>
+
+                                    </div>
+
+                                    <div className="grid justify-center">
+                                        <div className="inline-flex w-64 h-1 bg-indigo-500 rounded-full"></div>
+                                    </div>
+
+                                    <div className="grid place-items-center text-xl py-2 gap-2 w-full mb-4">
+                                        {ais.map(ai => { return <div>-{ai}</div> })}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="opacity-25 fixed inset-0 z-40 h-screen bg-black md:hidden"></div>
+                    </div>
+                ) : null}
+
             </div>
 
         </div>
